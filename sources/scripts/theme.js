ea5eaa5d989670858c2b1d38d70e7b16a2cd8be7
Devine Lu Linvega <aliceffekt@gmail.com>
2018-01-12 16:48:18 +1300

    Made theme more independant.

--- sources/scripts/theme.js
+++ sources/scripts/theme.js
@@ -1,99 +1,70 @@
 function Theme()
 {
-  this.el = document.createElement("style");
-  this.active = null;
+  var app = this;
 
-  this.default = { background: "#000",f_high: "white",f_med: "#a93232",f_low: "#cccccc",f_inv: "#a93232",b_high: "#ffffff",b_med: "#000000",b_low: "#333333",b_inv: "#a93232"}
+  this.el = document.createElement("style");
+  this.el.type = 'text/css';
+  this.default = { background: "#222", f_high: "#fff", f_med: "#777", f_low: "#444", f_inv: "#000", b_high: "#000", b_med: "#affec7", b_low: "#000", b_inv: "#affec7" }
+  this.active = this.default;
 
   this.start = function()
   {
-    document.body.appendChild(this.el)
-    
-    if(localStorage.theme && is_json(localStorage.theme)){
-      console.log("Theme","Found in localStorage")
-      this.install(JSON.parse(localStorage.theme));  
-    }
-    else{
-      console.log("Theme","Creating new")
-      this.install(this.default);
-    }
+    this.load(localStorage.theme ? localStorage.theme : this.default);
+    window.addEventListener('dragover',this.drag_enter);
+    window.addEventListener('drop', this.drag);
+    document.head.appendChild(this.el)
   }
 
-  this.save = function()
+  this.load = function(t)
   {
-    localStorage.setItem("theme", JSON.stringify(this.active));
-    console.log("Theme","Saved");
-  }
+    var theme = is_json(t) ? JSON.parse(t) : t;
 
-  this.load = function(theme_str)
-  {
-    if(is_json(theme_str)){
-      this.install(JSON.parse(theme_str));
-    }
-    console.log("Theme","Loaded");
-  }
+    if(!theme.background){ return; }
 
-  this.install = function(theme)
-  {
-    var html = "";
+    var css = `
+    :root {
+      --background: ${theme.background};
+      --f_high: ${theme.f_high};
+      --f_med: ${theme.f_med};
+      --f_low: ${theme.f_low};
+      --f_inv: ${theme.f_inv};
+      --b_high: ${theme.b_high};
+      --b_med: ${theme.b_med};
+      --b_low: ${theme.b_low};
+      --b_inv: ${theme.b_inv};
+    }`;
 
     this.active = theme;
-
-    html += "body { background:"+theme.background+" !important }\n";
-    html += ".fh { color:"+theme.f_high+" !important; stroke:"+theme.f_high+" !important }\n";
-    html += ".fm { color:"+theme.f_med+" !important ; stroke:"+theme.f_med+" !important }\n";
-    html += ".fl { color:"+theme.f_low+" !important ; stroke:"+theme.f_low+" !important }\n";
-    html += ".f_inv { color:"+theme.f_inv+" !important ; stroke:"+theme.f_inv+" !important }\n";
-    html += ".bh { background:"+theme.b_high+" !important; fill:"+theme.b_high+" !important }\n";
-    html += ".bm { background:"+theme.b_med+" !important ; fill:"+theme.b_med+" !important }\n";
-    html += ".bl { background:"+theme.b_low+" !important ; fill:"+theme.b_low+" !important }\n";
-    html += ".b_inv { background:"+theme.b_inv+" !important ; fill:"+theme.b_inv+" !important }\n";
-    html += ".b { background:"+theme.background+" !important }\n";
-
-    html += `
-
-svg .fill_red { fill:${theme.b_inv} !important }
-#board card.heart .face { color:${theme.b_inv} !important;}
-#board card.diamond .face { color:${theme.b_inv} !important;}
-
-svg .fill_white { fill:${theme.b_high} !important }
-#player a.escape { border:2px solid ${theme.b_high} !important; color: ${theme.b_high}; }
-#board card .face { background:${theme.b_high}}
-.card_11 .face .name,.card_13 .face .name,.card_15 .face .name,.card_17 .face .name { color:${theme.b_high};}
-#player .gage { color:${theme.b_high} }
-
-svg .fill_black { fill:${theme.b_med} !important }
-#board card.clove .face { color:${theme.b_med} !important;}
-#board card.spade .face { color:${theme.b_med} !important;}
-#board card.joker .face { color:${theme.b_med} !important;}
-
-svg .stroke_white { stroke:${theme.b_high} !important }
-svg .stroke_black { stroke:${theme.b_med} !important }
-svg .stroke_grey { stroke:${theme.f_low} !important }
-svg .fill_grey { fill:${theme.f_low} !important }
-
-#table card { background:${theme.b_low} !important}
-#player .gage .value .unit { color:${theme.b_low} !important}
-#player .gage .progress { background:${theme.b_low} !important }
-#tips { color: ${theme.b_low} !important }
-#tips .key { color: ${theme.b_high} !important }
-
-/* EXTRAS*/
-#player .gage.health .progress .bar { background:${theme.b_inv} !important  }
-#player .gage.shield .progress .bar { background:${theme.b_high} !important  }
-#player .gage.experience .progress .bar { background:${theme.b_med} !important  }
-#table card .shortcut { color:${theme.background} !important}
-
-`;
-
-    this.el.innerHTML = html;
-    this.save();
+    this.el.textContent = css;
+    localStorage.setItem("theme", JSON.stringify(theme));
   }
 
   this.reset = function()
   {
-    console.log("Theme","reset");
-    this.install(this.default);
+    this.load(this.default);
+  }
+
+  this.drag_enter = function(e)
+  {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+  }
+
+  this.drag = function(e)
+  {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var file = e.dataTransfer.files[0];
+
+    if(!file.name || !file.name.indexOf(".thm") < 0){ console.log("Theme","Not a theme"); return; }
+
+    var reader = new FileReader();
+    reader.onload = function(e){
+      app.load(e.target.result);
+    };
+    reader.readAsText(file);
   }
 
   function is_json(text)
