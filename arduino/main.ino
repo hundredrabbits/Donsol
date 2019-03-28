3c4868dc1e319cd2b3da269a24b0d63025710cd5
Devine Lu Linvega <aliceffekt@gmail.com>
2019-03-28 11:45:26 +0900

    *

--- arduino/main.ino
+++ arduino/main.ino
@@ -131,7 +131,7 @@ void drawworld() {
 // Deck
 
 int getCardType(int id){
-  return id / 13;
+  return floor(id / 13);
 }
 
 int getCardValue(int id){
@@ -140,8 +140,19 @@ int getCardValue(int id){
 
 char getCardName(int id){
   switch (id) {
-    case 13: return 'K';
-    case 12: return 'Q';
+    case 12: return 'K';
+    case 11:  return 'Q';
+    case 10:  return 'J';
+    case 9:  return '10';
+    case 8:  return '9';
+    case 7:  return '8';
+    case 6:  return '7';
+    case 5:  return '6';
+    case 4:  return '5';
+    case 3:  return '4';
+    case 2:  return '3';
+    case 1:  return '2';
+    case 0:  return 'A';
     default: return '?';
   }
   return id;
